ae2baca17e59ac5e27989b75bc72afca6b3708a4
Igor Minayeu <igor.minayeu@gmail.com>
2026-03-13 20:14:19 +0300

    Even or Odd 8kyu

---
+++ main.js
@@ -0,0 +1,9 @@
+// Even or Odd 8kyu
+
+// function evenOrOdd(number) {
+//     return number % 2  ? "Odd" : "Even"
+// }
+//
+// console.log(evenOrOdd(0))
+// console.log(evenOrOdd(7))
+// console.log(evenOrOdd(8))
