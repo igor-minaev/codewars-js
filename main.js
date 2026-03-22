ddb8cf7ab4abcd6c1721f02211ad6c7300288c02
Igor Minayeu <igor.minayeu@gmail.com>
2026-03-22 11:54:12 +0300

    Convert number to reversed array of digits 8kyu

--- main.js
+++ main.js
@@ -241,6 +241,15 @@
 //     return ((flower1 % 2) === 0 && (flower2 % 2) !== 0) || ((flower1 % 2) !== 0 && (flower2 % 2) === 0)
 // }
 
-function lovefunc(flower1, flower2) {
-    return flower1 % 2 !== flower2 % 2
+// function lovefunc(flower1, flower2) {
+//     return flower1 % 2 !== flower2 % 2
+// }
+
+// Convert number to reversed array of digits 8kyu
+
+function digitize(n) {
+    return n.toString().split('').reverse().map(Number)
 }
+
+
+console.log(digitize(35231))
